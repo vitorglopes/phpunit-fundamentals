f6ab01c1452129d34a3cad4da6d18e221e46f84b
Vitor Lopes <vitorlopeson@gmail.com>
2022-01-12 19:19:14 -0300

    Patterns - Arrange-Act-Assert / GivenWhenThen

--- leilao/teste-avaliador.php
+++ leilao/teste-avaliador.php
@@ -7,6 +7,8 @@ use Alura\Leilao\Service\Avaliador;
 
 require 'vendor/autoload.php';
 
+//ARRUMAR A CASA PARA O TESTE
+// Arrange / Given
 $leilao = new Leilao('FIAT 147 0KM');
 
 $maria = new Usuario('Maria');
@@ -16,8 +18,25 @@ $leilao->recebeLance(new Lance($joao, 2000));
 $leilao->recebeLance(new Lance($maria, 2500));
 
 $leiloeiro = new Avaliador();
+
+//EXECUTAR CÓDIGO A SER TESTADO
+// Act / When
+
 $leiloeiro->avalia($leilao);
 
 $maiorValor = $leiloeiro->getMaiorValor();
 
-echo $maiorValor;
+//VERIFICO SE A SAÍDA É A ESPERADA
+// Assert / Then
+
+$valorEsperado = 2500;
+
+if ($valorEsperado == $maiorValor) 
+{
+    echo 'Teste OK';
+} else {
+    echo 'Teste Falhou';
+}
+
+//ARRANGE-ACT-ASSERT -> http://wiki.c2.com/?ArrangeActAssert
+//GivenWhenThen -> https://martinfowler.com/bliki/GivenWhenThen.html
